08f2554f70ac2dab7a3daac9cf0b795f17b489e2
Jedsada-1339 <jedsada.si@kkumail.com>
2025-05-08 14:08:37 +0700

    upload exercise10

---
+++ excercise10/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    colors: {
+      'gay': '#BA4CE4',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
+}
